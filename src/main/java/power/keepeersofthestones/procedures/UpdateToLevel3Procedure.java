52bcb7ee27548a7f0f0eaa1cdb832cea697267ba
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-05-10 19:20:54 +0300

    Lots of changes

--- src/main/java/power/keepeersofthestones/procedures/UpdateToLevel3Procedure.java
+++ src/main/java/power/keepeersofthestones/procedures/UpdateToLevel3Procedure.java
@@ -6,9 +6,13 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import java.util.function.Supplier;
 import java.util.Map;
+import java.util.Iterator;
 
 public class UpdateToLevel3Procedure {
 	public static void execute(Entity entity) {
@@ -36,6 +40,15 @@ public class UpdateToLevel3Procedure {
 					&& _current.get() instanceof Map _slots) {
 				((Slot) _slots.get(1)).remove(16);
 				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("power:maximum_power"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					Iterator _iterator = _ap.getRemainingCriteria().iterator();
+					while (_iterator.hasNext())
+						_player.getAdvancements().award(_adv, (String) _iterator.next());
+				}
 			}
 		}
 	}
