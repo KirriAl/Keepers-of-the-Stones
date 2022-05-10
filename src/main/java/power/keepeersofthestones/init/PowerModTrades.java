01bd59825132aa052e87c081f61ecbf84fcb0a15
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-05-10 19:55:36 +0300

    Now you can buy elemental coins from the librarian

---
+++ src/main/java/power/keepeersofthestones/init/PowerModTrades.java
@@ -0,0 +1,31 @@
+
+/*
+*    MCreator note: This file will be REGENERATED on each build.
+*/
+package power.keepeersofthestones.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.common.BasicItemListing;
+
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.VillagerProfession;
+
+import java.util.List;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PowerModTrades {
+	@SubscribeEvent
+	public static void registerTrades(VillagerTradesEvent event) {
+		Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+		if (event.getType() == VillagerProfession.LIBRARIAN) {
+			trades.get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 32), new ItemStack(Items.BOOK),
+					new ItemStack(PowerModItems.ELEMENTAL_COIN.get()), 1, 100, 0.55f));
+		}
+	}
+}
