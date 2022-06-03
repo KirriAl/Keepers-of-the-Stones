46481248944513a74ee936a64bb68834de5a006b
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-06-04 01:48:27 +0300

    Add an element base (add effect, remove effect, localization, etc.) #12

--- src/main/java/power/keepeersofthestones/item/SandStoneItem.java
+++ src/main/java/power/keepeersofthestones/item/SandStoneItem.java
@@ -1,11 +1,17 @@
 
 package power.keepeersofthestones.item;
 
+import power.keepeersofthestones.procedures.SandStoneUseProcedure;
+
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
 
 public class SandStoneItem extends Item {
 	public SandStoneItem() {
@@ -25,5 +31,17 @@ public class SandStoneItem extends Item {
 	@Override
 	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 		return 0F;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+
+		SandStoneUseProcedure.execute(entity);
+		return ar;
 	}
 }
