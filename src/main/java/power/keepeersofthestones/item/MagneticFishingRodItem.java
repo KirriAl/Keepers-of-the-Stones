ceb3d3643317479bebefde5b918d975fc1ecb714
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-07-15 12:45:59 +0300

    Fix render magnetic fishing rod

--- src/main/java/power/keepeersofthestones/item/MagneticFishingRodItem.java
+++ src/main/java/power/keepeersofthestones/item/MagneticFishingRodItem.java
@@ -2,53 +2,25 @@
 package power.keepeersofthestones.item;
 
 import power.keepeersofthestones.procedures.MagneticPowerUseProcedure;
+import power.keepeersofthestones.init.PowerModItems;
 
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.core.BlockPos;
+import net.minecraft.stats.Stats;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.sounds.SoundEvents;
 
-import java.util.List;
-
-import com.google.common.collect.Multimap;
-import com.google.common.collect.ImmutableMultimap;
-
-public class MagneticFishingRodItem extends Item {
+public class MagneticFishingRodItem extends FishingRodItem {
 	public MagneticFishingRodItem() {
 		super(new Item.Properties().tab(null).durability(5000).fireResistant());
-	}
-
-	@Override
-	public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
-		return List.of().contains(blockstate.getBlock()) ? 4f : 1;
-	}
-
-	@Override
-	public boolean mineBlock(ItemStack itemstack, Level world, BlockState blockstate, BlockPos pos, LivingEntity entity) {
-		itemstack.hurtAndBreak(1, entity, i -> i.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-		return true;
-	}
-
-	@Override
-	public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
-		itemstack.hurtAndBreak(2, entity, i -> i.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-		return true;
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		MagneticPowerUseProcedure.execute(world, entity, ar.getObject());
-		return ar;
 	}
 
 	@Override
@@ -57,16 +29,41 @@ public class MagneticFishingRodItem extends Item {
 	}
 
 	@Override
-	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot equipmentSlot) {
-		if (equipmentSlot == EquipmentSlot.MAINHAND) {
-			ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-			builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
-			builder.put(Attributes.ATTACK_DAMAGE,
-					new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", 2f, AttributeModifier.Operation.ADDITION));
-			builder.put(Attributes.ATTACK_SPEED,
-					new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
-			return builder.build();
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		ItemStack itemstack = entity.getItemInHand(hand);
+		if (entity.fishing != null) {
+			if (!world.isClientSide()) {
+				itemstack.hurtAndBreak(entity.fishing.retrieve(itemstack), entity, i -> i.broadcastBreakEvent(hand));
+			}
+			world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.FISHING_BOBBER_RETRIEVE, SoundSource.NEUTRAL, 1,
+					0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+			world.gameEvent(entity, GameEvent.FISHING_ROD_REEL_IN, entity);
+		} else {
+			world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.FISHING_BOBBER_THROW, SoundSource.NEUTRAL, 0.5f,
+					0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+			if (!world.isClientSide()) {
+				int k = EnchantmentHelper.getFishingSpeedBonus(itemstack);
+				int j = EnchantmentHelper.getFishingLuckBonus(itemstack);
+				world.addFreshEntity(new FishingHook(entity, world, j, k) {
+					@Override
+					public boolean shouldStopFishing(Player player) {
+						if (!player.isRemoved() && player.isAlive()
+								&& (player.getMainHandItem().is(PowerModItems.MAGNETIC_FISHING_ROD.get())
+										|| player.getOffhandItem().is(PowerModItems.MAGNETIC_FISHING_ROD.get()))
+								&& !(this.distanceToSqr(player) > 1024)) {
+							return false;
+						} else {
+							this.discard();
+							return true;
+						}
+					}
+				});
+			}
+			entity.awardStat(Stats.ITEM_USED.get(this));
+			world.gameEvent(entity, GameEvent.FISHING_ROD_CAST, entity);
 		}
-		return super.getDefaultAttributeModifiers(equipmentSlot);
+
+		MagneticPowerUseProcedure.execute(world, entity, itemstack);
+		return InteractionResultHolder.sidedSuccess(itemstack, world.isClientSide());
 	}
 }
