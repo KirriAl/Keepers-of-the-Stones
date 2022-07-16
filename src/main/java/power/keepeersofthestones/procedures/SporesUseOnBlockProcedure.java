38ed767f29536a8263aa158587fda1f213fe96be
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-07-16 11:02:35 +0300

    Fix spores #19

--- src/main/java/power/keepeersofthestones/procedures/SporesUseOnBlockProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/SporesUseOnBlockProcedure.java
@@ -7,9 +7,9 @@ import net.minecraft.core.BlockPos;
 public class SporesUseOnBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if (Math.random() < 0.5) {
-			world.setBlock(new BlockPos(x, y, z), Blocks.BROWN_MUSHROOM.defaultBlockState(), 3);
+			world.setBlock(new BlockPos(x, y + 1, z), Blocks.BROWN_MUSHROOM.defaultBlockState(), 3);
 		} else if (Math.random() >= 0.5) {
-			world.setBlock(new BlockPos(x, y, z), Blocks.RED_MUSHROOM.defaultBlockState(), 3);
+			world.setBlock(new BlockPos(x, y + 1, z), Blocks.RED_MUSHROOM.defaultBlockState(), 3);
 		}
 	}
 }
