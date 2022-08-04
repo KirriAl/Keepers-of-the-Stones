4196531941a93939d4e05f61b754c6038a2a08e5
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-08-04 20:19:45 +0300

    Fixed the effect of the mass infection spell

--- src/main/java/power/keepeersofthestones/procedures/MAssInfectionTickProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/MAssInfectionTickProcedure.java
@@ -1,10 +1,7 @@
 package power.keepeersofthestones.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.particles.ParticleTypes;
 
@@ -16,10 +13,8 @@ public class MAssInfectionTickProcedure {
 			Entity _ent = entity;
 			if (!_ent.level.isClientSide() && _ent.getServer() != null)
 				_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4),
-						"effect give @e[dx=200, dy=200, dz=200] wither 1 1");
+						"effect give @e wither 1 1");
 		}
-		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 120, 2));
 		if (world instanceof ServerLevel _level)
 			_level.sendParticles(ParticleTypes.WITCH, x, y, z, 10, 1, 1, 1, 1);
 	}
