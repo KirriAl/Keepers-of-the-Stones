8a8ab8067999a2cccdd1fc8bdbf9bdd33418bcf2
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-08-09 21:10:36 +0300

    Improved procedures for rocket flight

--- src/main/java/power/keepeersofthestones/procedures/FlyOnEnceladusProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/FlyOnEnceladusProcedure.java
@@ -59,23 +59,48 @@ public class FlyOnEnceladusProcedure {
 			private void run() {
 				if (!entity.level.isClientSide())
 					entity.discard();
-				if (entity instanceof ServerPlayer _player && !_player.level.isClientSide()) {
-					ResourceKey<Level> destinationType = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("power:enceladus"));
-					if (_player.level.dimension() == destinationType)
-						return;
-					ServerLevel nextLevel = _player.server.getLevel(destinationType);
-					if (nextLevel != null) {
-						_player.connection.send(new ClientboundGameEventPacket(ClientboundGameEventPacket.WIN_GAME, 0));
-						_player.teleportTo(nextLevel, nextLevel.getSharedSpawnPos().getX(), nextLevel.getSharedSpawnPos().getY() + 1,
-								nextLevel.getSharedSpawnPos().getZ(), _player.getYRot(), _player.getXRot());
-						_player.connection.send(new ClientboundPlayerAbilitiesPacket(_player.getAbilities()));
-						for (MobEffectInstance _effectinstance : _player.getActiveEffects())
-							_player.connection.send(new ClientboundUpdateMobEffectPacket(_player.getId(), _effectinstance));
-						_player.connection.send(new ClientboundLevelEventPacket(1032, BlockPos.ZERO, 0, false));
+				new Object() {
+					private int ticks = 0;
+					private float waitTicks;
+					private LevelAccessor world;
+
+					public void start(LevelAccessor world, int waitTicks) {
+						this.waitTicks = waitTicks;
+						MinecraftForge.EVENT_BUS.register(this);
+						this.world = world;
 					}
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 400, 1));
+
+					@SubscribeEvent
+					public void tick(TickEvent.ServerTickEvent event) {
+						if (event.phase == TickEvent.Phase.END) {
+							this.ticks += 1;
+							if (this.ticks >= this.waitTicks)
+								run();
+						}
+					}
+
+					private void run() {
+						if (entity instanceof ServerPlayer _player && !_player.level.isClientSide()) {
+							ResourceKey<Level> destinationType = ResourceKey.create(Registry.DIMENSION_REGISTRY,
+									new ResourceLocation("power:enceladus"));
+							if (_player.level.dimension() == destinationType)
+								return;
+							ServerLevel nextLevel = _player.server.getLevel(destinationType);
+							if (nextLevel != null) {
+								_player.connection.send(new ClientboundGameEventPacket(ClientboundGameEventPacket.WIN_GAME, 0));
+								_player.teleportTo(nextLevel, nextLevel.getSharedSpawnPos().getX(), nextLevel.getSharedSpawnPos().getY() + 1,
+										nextLevel.getSharedSpawnPos().getZ(), _player.getYRot(), _player.getXRot());
+								_player.connection.send(new ClientboundPlayerAbilitiesPacket(_player.getAbilities()));
+								for (MobEffectInstance _effectinstance : _player.getActiveEffects())
+									_player.connection.send(new ClientboundUpdateMobEffectPacket(_player.getId(), _effectinstance));
+								_player.connection.send(new ClientboundLevelEventPacket(1032, BlockPos.ZERO, 0, false));
+							}
+						}
+						if (entity instanceof LivingEntity _entity)
+							_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 400, 1));
+						MinecraftForge.EVENT_BUS.unregister(this);
+					}
+				}.start(world, 20);
 				MinecraftForge.EVENT_BUS.unregister(this);
 			}
 		}.start(world, 200);
