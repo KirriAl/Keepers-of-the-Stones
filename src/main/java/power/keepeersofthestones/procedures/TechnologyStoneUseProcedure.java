a4d650449220831d7f917e3aff3ba13673289e71
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-08-22 15:45:50 +0300

    Fixes #100

--- src/main/java/power/keepeersofthestones/procedures/TechnologyStoneUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/TechnologyStoneUseProcedure.java
@@ -5,6 +5,7 @@ import power.keepeersofthestones.init.PowerModMobEffects;
 import power.keepeersofthestones.init.PowerModItems;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -15,19 +16,22 @@ public class TechnologyStoneUseProcedure {
 			return;
 		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PowerModItems.TECHNOLOGY_STONE
 				.get()) {
-			if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
-				if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_TECHNOLOGY_STONE.get()) : false)) {
-					if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-							.orElse(new PowerModVariables.PlayerVariables())).power_level == 2) {
-						if (entity instanceof LivingEntity _entity)
-							_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 18000, 0, (false), (false)));
-					} else if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-							.orElse(new PowerModVariables.PlayerVariables())).power_level >= 3) {
-						if (entity instanceof LivingEntity _entity)
-							_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 24000, 0, (false), (false)));
-					} else {
-						if (entity instanceof LivingEntity _entity)
-							_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 12000, 0, (false), (false)));
+			if ((entity instanceof LivingEntity _livEnt ? _livEnt.getArmorValue() : 0) == 0 && entity instanceof Player) {
+				if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+						.orElse(new PowerModVariables.PlayerVariables())).active) {
+					if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_TECHNOLOGY_STONE.get()) : false)) {
+						if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+								.orElse(new PowerModVariables.PlayerVariables())).power_level == 2) {
+							if (entity instanceof LivingEntity _entity)
+								_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 18000, 0, (false), (false)));
+						} else if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+								.orElse(new PowerModVariables.PlayerVariables())).power_level >= 3) {
+							if (entity instanceof LivingEntity _entity)
+								_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 24000, 0, (false), (false)));
+						} else {
+							if (entity instanceof LivingEntity _entity)
+								_entity.addEffect(new MobEffectInstance(PowerModMobEffects.TECHNOLOGY_MASTER.get(), 12000, 0, (false), (false)));
+						}
 					}
 				}
 			}
