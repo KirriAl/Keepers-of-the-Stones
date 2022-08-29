344a9fe6c9e3897e9b3894762dc6efca70579957
MagicalAlexey <animif220@gmail.com>
2022-08-29 16:15:06 +0300

    Changed the mechanics of destructive power

--- src/main/java/power/keepeersofthestones/procedures/DestructionPowerUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/DestructionPowerUseProcedure.java
@@ -1,40 +1,64 @@
 package power.keepeersofthestones.procedures;
 
+import power.keepeersofthestones.init.PowerModItems;
+
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.client.Minecraft;
 
 public class DestructionPowerUseProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.setBlock(new BlockPos(x, y, z), Blocks.ROOTED_DIRT.defaultBlockState(), 3);
-		new Object() {
-			private int ticks = 0;
-			private float waitTicks;
-			private LevelAccessor world;
-
-			public void start(LevelAccessor world, int waitTicks) {
-				this.waitTicks = waitTicks;
-				MinecraftForge.EVENT_BUS.register(this);
-				this.world = world;
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PowerModItems.DESTRUCTION_POWER
+				.get()) {
+			if (world.isClientSide())
+				Minecraft.getInstance().gameRenderer.displayItemActivation(itemstack);
+			if (entity instanceof Player _player)
+				_player.getCooldowns().addCooldown(itemstack.getItem(), 1200);
+			if (entity instanceof Player _player) {
+				_player.getAbilities().invulnerable = (true);
+				_player.onUpdateAbilities();
 			}
+			if (world instanceof Level _level && !_level.isClientSide())
+				_level.explode(null, x, y, z, 20, Explosion.BlockInteraction.DESTROY);
+			new Object() {
+				private int ticks = 0;
+				private float waitTicks;
+				private LevelAccessor world;
 
-			@SubscribeEvent
-			public void tick(TickEvent.ServerTickEvent event) {
-				if (event.phase == TickEvent.Phase.END) {
-					this.ticks += 1;
-					if (this.ticks >= this.waitTicks)
-						run();
+				public void start(LevelAccessor world, int waitTicks) {
+					this.waitTicks = waitTicks;
+					MinecraftForge.EVENT_BUS.register(this);
+					this.world = world;
 				}
-			}
 
-			private void run() {
-				world.setBlock(new BlockPos(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-				MinecraftForge.EVENT_BUS.unregister(this);
-			}
-		}.start(world, 400);
+				@SubscribeEvent
+				public void tick(TickEvent.ServerTickEvent event) {
+					if (event.phase == TickEvent.Phase.END) {
+						this.ticks += 1;
+						if (this.ticks >= this.waitTicks)
+							run();
+					}
+				}
+
+				private void run() {
+					if (entity instanceof Player _player) {
+						_player.getAbilities().invulnerable = (false);
+						_player.onUpdateAbilities();
+					}
+					MinecraftForge.EVENT_BUS.unregister(this);
+				}
+			}.start(world, 20);
+		}
 	}
 }
