d9fb7a922031c766a0df27298d26bc9666a423cc
MagicalAlexey <animif220@gmail.com>
2022-09-10 13:46:21 +0300

    More fixes and improvements

--- src/main/java/power/keepeersofthestones/procedures/MusicUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/MusicUseProcedure.java
@@ -2,11 +2,7 @@ package power.keepeersofthestones.procedures;
 
 import power.keepeersofthestones.network.PowerModVariables;
 import power.keepeersofthestones.init.PowerModMobEffects;
-import power.keepeersofthestones.init.PowerModItems;
 
-import net.minecraftforge.items.ItemHandlerHelper;
-
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -34,10 +30,11 @@ public class MusicUseProcedure {
 		}
 		if (entity instanceof Player _player)
 			_player.closeContainer();
-		if (entity instanceof Player _player) {
-			ItemStack _setstack = new ItemStack(PowerModItems.PINCH_OF_GOLDEN_DUST.get());
-			_setstack.setCount(1);
-			ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+		{
+			Entity _ent = entity;
+			if (!_ent.level.isClientSide() && _ent.getServer() != null)
+				_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4),
+						"give @s power:pinch_of_golden_dust{Enchantments:[{id:binding_curse,lvl:1},{id:vanishing_curse,lvl:1}]}");
 		}
 	}
 }
