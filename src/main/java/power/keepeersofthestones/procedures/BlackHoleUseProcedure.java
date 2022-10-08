87eb2f1a6d720bf89afd52c96203ee0d3476ce08
MagicalAlexey <animif220@gmail.com>
2022-10-09 00:40:09 +0300

    Fixes some crushes

--- src/main/java/power/keepeersofthestones/procedures/BlackHoleUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/BlackHoleUseProcedure.java
@@ -2,40 +2,11 @@ package power.keepeersofthestones.procedures;
 
 import power.keepeersofthestones.init.PowerModBlocks;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.common.MinecraftForge;
-
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
 public class BlackHoleUseProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		class WaitHandler1 {
-			private int ticks = 0;
-			private float waitTicks;
-			private LevelAccessor world;
-
-			public void start(LevelAccessor world, int waitTicks) {
-				this.waitTicks = waitTicks;
-				this.world = world;
-				MinecraftForge.EVENT_BUS.register(WaitHandler1.this);
-			}
-
-			@SubscribeEvent
-			public void tick(TickEvent.ServerTickEvent event) {
-				if (event.phase == TickEvent.Phase.END) {
-					WaitHandler1.this.ticks += 1;
-					if (WaitHandler1.this.ticks >= WaitHandler1.this.waitTicks)
-						run();
-				}
-			}
-
-			private void run() {
-				MinecraftForge.EVENT_BUS.unregister(WaitHandler1.this);
-				world.setBlock(new BlockPos(x, y + 1, z), PowerModBlocks.BLACK_HOLE_BLOCK.get().defaultBlockState(), 3);
-			}
-		}
-		new WaitHandler1().start(world, 3);
+		world.setBlock(new BlockPos(x, y + 1, z), PowerModBlocks.BLACK_HOLE_BLOCK.get().defaultBlockState(), 3);
 	}
 }
--- src/main/java/power/keepeersofthestones/procedures/GoldenShieldUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/GoldenShieldUseProcedure.java
@@ -28,36 +28,6 @@ public class GoldenShieldUseProcedure {
 				Minecraft.getInstance().gameRenderer.displayItemActivation(itemstack);
 			if (entity instanceof Player _player)
 				_player.getCooldowns().addCooldown(itemstack.getItem(), 400);
-			class WaitHandler7 {
-				private int ticks = 0;
-				private float waitTicks;
-				private LevelAccessor world;
-
-				public void start(LevelAccessor world, int waitTicks) {
-					this.waitTicks = waitTicks;
-					this.world = world;
-					MinecraftForge.EVENT_BUS.register(WaitHandler7.this);
-				}
-
-				@SubscribeEvent
-				public void tick(TickEvent.ServerTickEvent event) {
-					if (event.phase == TickEvent.Phase.END) {
-						WaitHandler7.this.ticks += 1;
-						if (WaitHandler7.this.ticks >= WaitHandler7.this.waitTicks)
-							run();
-					}
-				}
-
-				private void run() {
-					MinecraftForge.EVENT_BUS.unregister(WaitHandler7.this);
-					if (world instanceof ServerLevel _level)
-						_level.getServer().getCommands()
-								.performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "",
-										Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-										"fill ~-2 ~ ~-2 ~2 ~4 ~2 gold_block outline");
-				}
-			}
-			new WaitHandler7().start(world, 3);
 			class WaitHandler9 {
 				private int ticks = 0;
 				private float waitTicks;
@@ -84,10 +54,41 @@ public class GoldenShieldUseProcedure {
 						_level.getServer().getCommands()
 								.performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "",
 										Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-										"fill ~-2 ~ ~-2 ~2 ~4 ~2 air outline");
+										"fill ~-2 ~ ~-2 ~2 ~4 ~2 gold_block outline");
+					class WaitHandler8 {
+						private int ticks = 0;
+						private float waitTicks;
+						private LevelAccessor world;
+
+						public void start(LevelAccessor world, int waitTicks) {
+							this.waitTicks = waitTicks;
+							this.world = world;
+							MinecraftForge.EVENT_BUS.register(WaitHandler8.this);
+						}
+
+						@SubscribeEvent
+						public void tick(TickEvent.ServerTickEvent event) {
+							if (event.phase == TickEvent.Phase.END) {
+								WaitHandler8.this.ticks += 1;
+								if (WaitHandler8.this.ticks >= WaitHandler8.this.waitTicks)
+									run();
+							}
+						}
+
+						private void run() {
+							MinecraftForge.EVENT_BUS.unregister(WaitHandler8.this);
+							if (world instanceof ServerLevel _level)
+								_level.getServer().getCommands()
+										.performPrefixedCommand(
+												new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "",
+														Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+												"fill ~-2 ~ ~-2 ~2 ~4 ~2 air outline");
+						}
+					}
+					new WaitHandler8().start(world, 400);
 				}
 			}
-			new WaitHandler9().start(world, 400);
+			new WaitHandler9().start(world, 3);
 		}
 	}
 }
