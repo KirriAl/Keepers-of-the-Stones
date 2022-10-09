7c7eb9749481a83d661fad24bc82d16fd05d7ec3
MagicalAlexey <animif220@gmail.com>
2022-10-10 01:11:12 +0300

    Added holes models (fix translucent)

--- src/main/java/power/keepeersofthestones/block/VacuumWebBlockBlock.java
+++ src/main/java/power/keepeersofthestones/block/VacuumWebBlockBlock.java
@@ -3,13 +3,15 @@ package power.keepeersofthestones.block;
 
 import power.keepeersofthestones.procedures.VacuumTPProcedure;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.PressurePlateBlock;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
@@ -19,15 +21,26 @@ import net.minecraft.core.BlockPos;
 import java.util.List;
 import java.util.Collections;
 
-public class VacuumWebBlockBlock extends PressurePlateBlock {
+public class VacuumWebBlockBlock extends Block {
 	public VacuumWebBlockBlock() {
-		super(Sensitivity.MOBS, BlockBehaviour.Properties.of(Material.SCULK).sound(SoundType.SCULK_SENSOR).strength(8f, 50f).noCollission()
-				.friction(0.1f).speedFactor(0f).jumpFactor(0f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false).dynamicShape());
+		super(BlockBehaviour.Properties.of(Material.SCULK).sound(SoundType.SCULK_SENSOR).strength(8f, 50f).noCollission().friction(0.1f)
+				.speedFactor(0f).jumpFactor(0f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+	}
+
+	@Override
+	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return true;
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 0;
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+
+		return box(0, 0, 0, 16, 1, 16);
 	}
 
 	@Override
