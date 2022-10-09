111a020b0bab322f2ab4c4dc2c57529c8b188bd5
MagicalAlexey <animif220@gmail.com>
2022-10-10 01:18:38 +0300

    Update mod to 2.7.5

--- src/main/java/power/keepeersofthestones/procedures/VacuumWebUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/VacuumWebUseProcedure.java
@@ -2,11 +2,40 @@ package power.keepeersofthestones.procedures;
 
 import power.keepeersofthestones.init.PowerModBlocks;
 
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
 public class VacuumWebUseProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.setBlock(new BlockPos(x, y + 1, z), PowerModBlocks.VACUUM_WEB_BLOCK.get().defaultBlockState(), 3);
+		class WaitHandlerVacuumWebUse1 {
+			private int ticks = 0;
+			private float waitTicks;
+			private LevelAccessor world;
+
+			public void start(LevelAccessor world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				this.world = world;
+				MinecraftForge.EVENT_BUS.register(WaitHandlerVacuumWebUse1.this);
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					WaitHandlerVacuumWebUse1.this.ticks += 1;
+					if (WaitHandlerVacuumWebUse1.this.ticks >= WaitHandlerVacuumWebUse1.this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				MinecraftForge.EVENT_BUS.unregister(WaitHandlerVacuumWebUse1.this);
+				world.setBlock(new BlockPos(x, y + 1, z), PowerModBlocks.VACUUM_WEB_BLOCK.get().defaultBlockState(), 3);
+			}
+		}
+		new WaitHandlerVacuumWebUse1().start(world, 3);
 	}
 }
